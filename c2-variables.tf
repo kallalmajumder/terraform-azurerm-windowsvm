4d7e202da12576190834d93f46085a2d2662d966
Kallal Majumder <ikallal@kallals-air.home>
2022-01-26 19:07:31 +0000

    Second Commit

--- c2-variables.tf
+++ c2-variables.tf
@@ -1,34 +1,30 @@
-# Input variable definitions
-variable "location" {
-  description = "The Azure Region in which all resources groups should be created."
-  type        = string
-}
 variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
+  type = string
 }
-variable "storage_account_name" {
-  description = "The name of the storage account"
-  type        = string
+variable "resource_group_location" {
+  type = string
 }
-variable "storage_account_tier" {
-  description = "Storage Account Tier"
-  type        = string
+variable "vnet_name" {
+  type = string
 }
-variable "storage_account_replication_type" {
-  description = "Storage Account Replication Type"
-  type        = string
+variable "vnet_address_space" {
+  type = list(string)
 }
-variable "storage_account_kind" {
-  description = "Storage Account Kind"
-  type        = string
+variable "snet_name" {
+  type = string
 }
-variable "static_website_index_document" {
-  description = "static website index document"
-  type        = string
+variable "snet_address_space" {
+  type = list(string)
 }
-variable "static_website_error_404_document" {
-  description = "static website error 404 document"
-  type        = string
+variable "publicip_name" {
+  type = string
 }
-
+variable "publicip_allocation" {
+  type = string
+}
+variable "network_interface_name" {
+  type = string
+}
+variable "vm_name" {
+  type = string
+}
